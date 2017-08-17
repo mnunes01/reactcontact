d943b3f27a05116f4eee00f3a84463555a8e7bcd
Mario Nunes <mario.nunes@coral.co.uk>
2017-08-17 16:58:48 +0200

    reverting back to browserrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MemoryRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css'
 
 import Header from './pages/header/'
