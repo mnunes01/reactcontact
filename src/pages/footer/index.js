5bdf23d30077ffb52b0ccbd0925a43472c50f872
Mario Nunes <mario.nunes@coral.co.uk>
2017-08-17 16:15:54 +0200

    removing trailing spaces

--- src/pages/footer/index.js
+++ src/pages/footer/index.js
@@ -4,7 +4,7 @@ export default class Footer extends React.Component {
   render () {
     return (
       <div className='footer'>
-        <small>Running application in <b>{process.env.NODE_ENV}</b> mode. <br />@Mario Nunes 08/2017 Hello:<a href='mailto:mnunes01@hotmail.com' rel='noopener noreferrer' target='_blank'>mnunes01@hotmail.com</a></small>        
+        <small>Running application in <b>{process.env.NODE_ENV}</b> mode. <br />@Mario Nunes 08/2017 Hello:<a href='mailto:mnunes01@hotmail.com' rel='noopener noreferrer' target='_blank'>mnunes01@hotmail.com</a></small>
       </div>
     )
   }
